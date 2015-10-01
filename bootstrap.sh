e95442b1b81d5806f90e52cdae5b9fbdbeb055f3
Pierrick Roger Mele <pierrick.rogermele@me.com>
2015-10-01 17:02:55 +0200

    Split bootstrap file into two files.

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# FIXME R not installed.
-yum install R.x86_64
-
-# FIXME Galaxy installed as root.
-git clone https://github.com/galaxyproject/galaxy
-cd galaxy
-git checkout -b master origin/master
