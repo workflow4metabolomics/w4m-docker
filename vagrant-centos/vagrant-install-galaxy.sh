4485c141d9cd97a8fa2eedb6d496b7ef39ce6121
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2015-10-02 16:07:09 +0200

    Trying to run Galaxy on Centos vm.

--- vagrant-centos/vagrant-install-galaxy.sh
+++ vagrant-centos/vagrant-install-galaxy.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+# Install or update Galaxy
 if [ -d galaxy ] ; then
+
+	# Update Galaxy
 	cd galaxy
 	git pull
+
 else
+
+	# Install Galaxy
 	git clone https://github.com/galaxyproject/galaxy
 	cd galaxy
 	git checkout -b master origin/master
+
+	# Write .ini
+	cp /vagrant/galaxy.ini config/.
 fi
