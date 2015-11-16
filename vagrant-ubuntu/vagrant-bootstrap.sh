e8288b4974f2a96b5042af1a1cb6058cba22d6c2
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2015-11-16 01:16:36 +0100

    Try to install w4m-tool-lcmsmatching.

--- vagrant-ubuntu/vagrant-bootstrap.sh
+++ vagrant-ubuntu/vagrant-bootstrap.sh
@@ -4,5 +4,6 @@ apt-get update
 apt-get install -y r-base
 apt-get install -y git
 
-sh /vagrant/vagrant-run-galaxy.sh
-
+# Requirements for installing and testing w4m-tool-lcmsmatching
+apt-get install -y ant
+R -e "install.packages('getopt', dependencies = TRUE, repos='http://lib.ugent.be/CRAN/')"
--- vagrant-ubuntu/vagrant-install-galaxy.sh
+++ vagrant-ubuntu/vagrant-install-galaxy.sh
@@ -6,6 +6,7 @@ if [ -d galaxy ] ; then
 	# Update Galaxy
 	cd galaxy
 	git pull
+	cd ..
 
 else
 
@@ -16,4 +17,23 @@ else
 
 	# Write .ini
 	cp /vagrant/galaxy.ini config/.
+
+	cd ..
 fi
+
+# Install or update w4m tools
+mkdir tmp
+mkdir -p galaxy/tools/w4m
+for tool in w4m-tool-lcmsmatching ; do
+	cd tmp
+	git clone https://github.com/pierrickrogermele/$tool
+	cd $tool
+	git submodule init
+	git submodule update
+	ant dist # TODO change it for an "galaxy.install" target that takes a parameter -D GALAXY.INSTALL.DIR=...
+# TODO what about the dependencies of the tool (search-mz) ?
+	cd ../galaxy/tools/w4m
+	tar -xzf ../../../tmp/$tool/dist/*.tar.gz
+
+	# Create tool_conf.xml
+done
