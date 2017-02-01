7c1262e7639edda0b5a5907638da941db97d75f7
lecorguille <lecorguille@users.noreply.github.com>
2017-02-01 17:00:36 +0100

    remove vagrant folder

--- vagrant-ubuntu/vagrant-bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y r-base
-apt-get install -y git
-
-# Requirements for installing and testing w4m-tool-lcmsmatching
-#apt-get install -y ant
-#R -e "install.packages('getopt', dependencies = TRUE, repos='http://lib.ugent.be/CRAN/')"
